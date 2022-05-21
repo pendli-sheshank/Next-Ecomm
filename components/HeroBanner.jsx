32406b9f35551bb908a8f9db740340fa6ed4b8c3
sheshankp <sheshank3336@gmail.com>
2022-05-21 15:00:19 +0530

    image not showing

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { urlFor } from "../lib/client";
+
+const HeroBanner = ({ heroBanner }) => {
+  console.log("banner", heroBanner);
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText}</h1>
+        {heroBanner && (
+          <img
+            src={urlFor(heroBanner.image).url()}
+            alt="headphones"
+            className="hero-banner-image"
+          />
+        )}
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
